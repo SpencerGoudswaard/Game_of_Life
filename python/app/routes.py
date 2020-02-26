b8c72bbb3929174d651460eb7d98522a252f5b39
Spencer <Spencergoudswaard@hotmail.com>
2020-02-26 11:28:58 -0800

    Removed python files

--- python/app/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-from app import app
-
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Spencer'}
-    return render_template('writetest.html', title='Home', user=user)
